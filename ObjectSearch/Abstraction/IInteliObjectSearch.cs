9803910061cc8a832016979093e07997780d9e61
marianp <marianpatzke@gmail.com>
2019-03-03 13:18:52 +0100

    FieldFinder implementiert

---
+++ ObjectSearch/Abstraction/IInteliObjectSearch.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectSearch.Abstraction
+{
+	public interface IInteliObjectSearch
+	{
+		IEnumerable<TObj> Search<TObj>(IEnumerable<TObj> data, string search);
+	}
+}
---
+++ ObjectSearch/Abstraction/IObjectSearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectSearch.Abstraction
+{
+	public interface IObjectSearch
+	{
+		IEnumerable<SearchResult<TObj>> Search<TObj>(IEnumerable<TObj> data, 
+													 string[] searchStrings, SearchConfig config);		
+	}
+
+	public class SearchConfig
+	{
+		public ESearchMode SearchMode { get; set; }
+		public Type[] SearchTypes { get; set; }
+		public string[] IgnoreFields { get; set; }
+	}
+
+	public enum ESearchMode
+	{
+		And, Or
+	}
+
+	public class SearchResult<TObj>
+	{
+		public int Importance { get; set; }
+		public TObj Object { get; set; }
+	}
+}
