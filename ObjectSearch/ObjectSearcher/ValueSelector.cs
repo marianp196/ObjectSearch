e6b691fe73e66790ac545f4db27a1b4b74ce0a5f
marianp <marianpatzke@gmail.com>
2019-03-03 15:28:34 +0100

    ValueSelector hinzugefügt

---
+++ ObjectSearch/ObjectSearcher/ValueSelector.cs
@@ -0,0 +1,47 @@
+﻿using ObjectSearcher.ObjectSearcher;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectSearcher.ObjectSearcher
+{
+	public class ValueSelector
+	{
+		public IEnumerable<string> GetSearchableValues<TObj>(IEnumerable<FieldPath> fieldPaths, TObj objDta)
+		{
+			var searchableValues = new List<string>();
+			foreach (var fieldPath in fieldPaths)
+			{
+				searchableValues.AddRange(GetValuesFromObject<TObj>(objDta, fieldPath));
+			}
+			return searchableValues;
+		}
+
+		private IEnumerable<string> GetValuesFromObject<TObj>(TObj obj, FieldPath fieldPath)
+		{
+			var result = new List<string>();
+
+			if (fieldPath.Next.Count == 0)
+			{
+				var value = fieldPath.Property.GetValue(obj)?.ToString();
+				if(value != null && value != "")
+					result.Add(value);
+				return result;
+			}
+			else
+			{
+				var propValue = fieldPath.Property.GetValue(obj);
+				if (propValue == null)
+					return result;
+
+				foreach (var fieldChild in fieldPath.Next)
+				{
+					var valueList = GetValuesFromObject(propValue, fieldChild);
+					result.AddRange(valueList);
+				}
+			}
+
+			return result;
+		}
+	}
+}
